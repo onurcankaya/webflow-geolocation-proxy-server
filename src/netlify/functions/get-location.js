8380fb2482c5d54aded4acc627dec88ece40aef4
Onur Çankaya <onur.eren.cankaya@gmail.com>
2024-08-28 20:45:26 +0200

    initial commit

---
+++ src/netlify/functions/get-location.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function handler() {
+  const apiKey = process.env.IPAPI_API_KEY;
+  const url = `https://api.ipapi.com/api/check?access_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("data", data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch location data" }),
+    };
+  }
+}
