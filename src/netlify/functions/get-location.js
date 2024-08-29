4a1b9d977309f9746990a0358d18966df8785c4d
Onur Çankaya <onur.eren.cankaya@gmail.com>
2024-08-29 10:11:16 +0200

    use ip of visitor

--- src/netlify/functions/get-location.js
+++ src/netlify/functions/get-location.js
@@ -3,9 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export async function handler() {
+export async function handler(event) {
+  const ip =
+    event.headers["x-forwarded-for"] || event.headers["cf-connecting-ip"];
+
+  if (!ip) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "IP address not found" }),
+    };
+  }
+
+  console.log("ip", ip);
+
   const apiKey = process.env.IPAPI_API_KEY;
-  const url = `https://api.ipapi.com/api/check?access_key=${apiKey}`;
+  const url = `https://api.ipapi.com/api/${ip}?access_key=${apiKey}`;
 
   try {
     const response = await fetch(url);
